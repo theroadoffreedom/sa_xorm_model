b664c57ab390cc3cb3f287893998dc4c49bb08bc
busydeng <ccong.deng@gmail.com>
2019-07-07 22:29:15 +0800

    add report index

--- t_auto_finance_report_index.go
+++ t_auto_finance_report_index.go
@@ -1,10 +1,11 @@
 package sa_xorm_model
 
 type TAutoFinanceReportIndex struct {
-	Id         string `json:"id" xorm:"not null pk VARCHAR(128)"`
-	DataTime   int    `json:"data_time" xorm:"not null pk INT(10)"`
-	CheckTime  int    `json:"check_time" xorm:"INT(10)"`
-	CreateTime int    `json:"create_time" xorm:"INT(10)"`
-	State      string `json:"state" xorm:"VARCHAR(128)"`
-	ReportType string `json:"report_type" xorm:"not null pk VARCHAR(128)"`
+	Id             string `json:"id" xorm:"not null pk VARCHAR(128)"`
+	DataTime       int    `json:"data_time" xorm:"not null pk INT(10)"`
+	CheckTime      int    `json:"check_time" xorm:"INT(10)"`
+	CreateTime     int    `json:"create_time" xorm:"INT(10)"`
+	State          int    `json:"state" xorm:"INT(10)"`
+	ReportType     int    `json:"report_type" xorm:"not null pk INT(10)"`
+	ReportTimeType int    `json:"report_time_type" xorm:"INT(10)"`
 }
--- t_zcfzb_by_year.go
+++ t_zcfzb_by_year.go
@@ -4,5 +4,4 @@ type TZcfzbByYear struct {
 	Id       string `json:"id" xorm:"not null pk VARCHAR(128)"`
 	DataTime int    `json:"data_time" xorm:"not null pk INT(10)"`
 	Data     string `json:"data" xorm:"JSON"`
-	ReportId string `json:"report_id" xorm:"unique VARCHAR(128)"`
 }
