674919b1ad67cdb1284d4adee39eb8105263bdad
busydeng <ccong.deng@gmail.com>
2019-08-04 17:10:18 +0800

    update

--- t_stock.go
+++ t_stock.go
@@ -7,8 +7,6 @@ type TStock struct {
 	FullName                 string `json:"full_name" xorm:"VARCHAR(512)"`
 	RegisterAddress          string `json:"register_address" xorm:"VARCHAR(1024)"`
 	PublicTime               int    `json:"public_time" xorm:"INT(10)"`
-	TotalAShareCapital       int    `json:"total_a_share_capital" xorm:"INT(34)"`
-	CirculatingAShareCapital int    `json:"circulating_a_share_capital" xorm:"INT(32)"`
 	Region                   string `json:"region" xorm:"VARCHAR(128)"`
 	Province                 string `json:"province" xorm:"VARCHAR(128)"`
 	City                     string `json:"city" xorm:"VARCHAR(128)"`
@@ -17,4 +15,13 @@ type TStock struct {
 	CreateTime               int    `json:"create_time" xorm:"INT(10)"`
 	SearchTime               int    `json:"search_time" xorm:"INT(10)"`
 	CbCheckTime              int    `json:"cb_check_time" xorm:"INT(10)"`
+	CurrentPrice             string `json:"current_price" xorm:"DECIMAL(10,2)"`
+	High52wPrice             string `json:"high52w_price" xorm:"DECIMAL(10,2)"`
+	Low52wPrice              string `json:"low52w_price" xorm:"DECIMAL(10,2)"`
+	InfoCheckTime            int    `json:"info_check_time" xorm:"INT(10)"`
+	TotalAShareCapital       int64  `json:"total_a_share_capital" xorm:"BIGINT(20)"`
+	FreeMarketCapital        int64  `json:"free_market_capital" xorm:"BIGINT(20)"`
+	MarketCapital            int64  `json:"market_capital" xorm:"BIGINT(20)"`
+	FreeAShareCapital        int64  `json:"free_a_share_capital" xorm:"BIGINT(20)"`
+	CirculatingAShareCapital int64  `json:"circulating_a_share_capital" xorm:"BIGINT(20)"`
 }
